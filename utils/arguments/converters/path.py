83cd484a5cef87c6b3a2667189d2a3af65f0a54b
ggzor <30713864+ggzor@users.noreply.github.com>
2018-06-12 20:43:09 -0500

    Adding path.validate.valid_directoryname

--- utils/arguments/converters/path.py
+++ utils/arguments/converters/path.py
@@ -1,4 +1,6 @@
 from pathlib import Path
+from os import mkdir, rmdir
+from tempfile import TemporaryFile
 from utils.languageex import value_or_raise_if
 
 class validate:
@@ -6,7 +8,7 @@ class validate:
     def existing(path):
         return value_or_raise_if(
             not path.exists(), 
-                'The path "{}" doesn´t exist.'.format(path), 
+                "The path '{}' doesn´t exist.".format(path), 
             path
         )
 
@@ -14,6 +16,18 @@ class validate:
     def isdirectory(path):
         return value_or_raise_if(
             not path.is_dir(),
-                'The path "{}" is not a directory.'.format(path),
+                "The path '{}' is not a directory.".format(path),
             path
         )
+
+    @staticmethod
+    def valid_directoryname(name):
+        try:
+            mkdir(name)
+            p = Path(name)
+
+            if p.is_dir():   
+                rmdir(name)
+                return name
+        except:
+            raise Exception("The value '{}' is not a valid directory name.".format(name))
